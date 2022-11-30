9f1d48647dc0121ae4a1bcc1d971fb4d087e2bad
Anton Naumtsev <naumtsevanton@mail.ru>
2022-11-30 14:22:54 +0300

    added auth

--- main.py
+++ main.py
@@ -1,30 +1,49 @@
-from flask import Flask, render_template, session, redirect
+from flask import Flask, render_template, session, redirect, request
 from objects.room import Room
 import uuid
 from util import check_auth
+
+import random, string
+
+def randomword(length):
+   letters = string.ascii_lowercase
+   return ''.join(random.choice(letters) for i in range(length))
+
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "hse_2022"
+app.config['SECRET_KEY'] = randomword(40)
 
 
 rooms = {}
 
+users = {}
+
+
+class User:
+    def __init__(self, login, password, balance=123) -> None:
+        self.login = login
+        self.password = password
+        self.balance = balance
+
 
 @app.route('/')
 def index():
-    if 'user_id' not in session:
-        session['user_id'] = str(uuid.uuid4())
+    if not check_auth():
+        return redirect('/login')
 
     msg = session.get('message', '')
 
     if 'message' in session:
         session.pop('message')
 
-    return render_template('index.html', user_data=session, message=msg)
+    return render_template('index.html', user_data=users[session['user_id']], message=msg)
 
 
 @app.route('/start', methods=['GET'])
 def start():
-    check_auth()
+    if not check_auth():
+        return redirect('/login')
+    
     room_id = str(uuid.uuid4())
     rooms[room_id] = Room(room_id, user_id=session['user_id'])
     return redirect(f'/room/{room_id}')
@@ -32,7 +51,8 @@ def start():
 
 @app.route('/room/<string:room_id>', methods=['GET'])
 def playing_room(room_id):
-    check_auth()
+    if not check_auth():
+        return redirect('/login')
 
     if room_id not in rooms:
         return redirect('/')
@@ -47,7 +67,8 @@ def playing_room(room_id):
 
 @app.route('/room/<string:room_id>/hit', methods=['GET'])
 def hit(room_id):
-    check_auth()
+    if not check_auth():
+        return redirect('/login')
 
     if room_id not in rooms:
         return redirect('/')
@@ -64,7 +85,8 @@ def hit(room_id):
 
 @app.route('/room/<string:room_id>/stand', methods=['GET'])
 def stand(room_id):
-    check_auth()
+    if not check_auth():
+        return redirect('/login')
 
     if room_id not in rooms:
         return redirect('/')
@@ -80,7 +102,8 @@ def stand(room_id):
 
 @app.route('/room/<string:room_id>/quit', methods=['GET'])
 def quit(room_id):
-    check_auth()
+    if not check_auth():
+        return redirect('/login')
 
     if room_id not in rooms:
         return redirect('/')
@@ -98,6 +121,29 @@ def page_not_found(_):
     return render_template('page_not_found.html')
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if check_auth():
+        return redirect('/error')
+    
+    if request.method == 'GET':
+        return render_template('login.html')
+    else:
+        login = request.form['login']
+        password = request.form['password']
+    
+        if login in users:
+            user = users[login]
+
+            if user.password == password:
+                session['user_id'] = login
+                return redirect('/')
+            else:
+                return render_template('login.html', message='password is invalid') 
+        else:
+            users[login] = User(login, password)
+            session['user_id'] = login
+            return redirect('/') 
 
 @app.route('/logout', methods=['GET'])
 def logout():
@@ -106,4 +152,4 @@ def logout():
     return 'OK'
 
 
-app.run()
+app.run(port=1234)
